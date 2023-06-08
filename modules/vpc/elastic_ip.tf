7ac9aebe2e4399c87b449c1f5225b554c7d33bfa
Mateus Marques <mateusmarques1993@gmail.com>
2023-06-08 15:32:08 -0300

    Elastic IP - vpc parameter removal.

--- modules/vpc/elastic_ip.tf
+++ modules/vpc/elastic_ip.tf
@@ -1,7 +1,7 @@
 resource "aws_eip" "marques_elastic_ip" {
   count   = length(var.public_cidr_block)
 
-  vpc     = true
+  domain  = "vpc"
 
   tags = merge({Name = "${var.prefix_name}-elastic-ip"},
                {Elastic_IP = count.index},
