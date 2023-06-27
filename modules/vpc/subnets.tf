57e1e6c2359390557f3c1c4792959d788ecade89
Marques <mateusmarques1993@gmail.com>
2023-06-27 15:52:08 -0300

    Kubernetes subnets tags fi.

--- modules/vpc/subnets.tf
+++ modules/vpc/subnets.tf
@@ -7,7 +7,7 @@ resource "aws_subnet" "marques_public_subnet" {
   availability_zone       = var.azs[count.index]
 
   tags = merge ({Name = "${var.prefix_name}-public-vpc"},
-                {kubernetes.io/role/elb = 1},
+                {"kubernetes.io/role/elb" = "1"},
                 {Cidr_Block = substr(var.public_cidr_block[count.index], -6, -3)},
                 var.tags)
 }
@@ -20,7 +20,7 @@ resource "aws_subnet" "marques_private_subnet" {
   availability_zone = var.azs[count.index]
 
   tags = merge ({Name = "${var.prefix_name}-private-vpc"},
-                {kubernetes.io/role/internal-elb = 1},
+                {"kubernetes.io/role/internal-elb" = "1"},
                 {Cidr_Block = substr(var.private_cidr_block[count.index], -7, -3)},
                 var.tags)
 }
